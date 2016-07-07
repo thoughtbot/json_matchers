009ad00980dc8d7d5663dcdee384fb75ff225116
Dray Lacy <dray@envylabs.com>
2016-07-06 22:53:06 -0400

    Replace `mattr_accessor` with equivalent code
    Note that `mattr_accessor` also adds instance-level getter and setter
    methods which we haven't implemented in this case, but those aren't
    being used anyway so there's no need to keep them around.

--- lib/json_matchers.rb
+++ lib/json_matchers.rb
@@ -4,9 +4,15 @@ require "json_matchers/errors"
 require "active_support/all"
 
 module JsonMatchers
-  mattr_accessor :schema_root
+  @@schema_root = "#{Dir.pwd}/spec/support/api/schemas"
 
-  self.schema_root = "#{Dir.pwd}/spec/support/api/schemas"
+  def self.schema_root
+    @@schema_root
+  end
+
+  def self.schema_root=(value)
+    @@schema_root = value
+  end
 
   def self.path_to_schema(schema_name)
     Pathname(schema_root).join("#{schema_name}.json")
