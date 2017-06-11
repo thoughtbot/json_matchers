b71e05b11c9fe4d28a6108c0fc91f0572e700e7f
Justin Smestad <justin.smestad@gmail.com>
2017-06-10 18:28:22 -0600

    Fix RSpec matcher for matching against already parsed JSON

--- lib/json_matchers/payload.rb
+++ lib/json_matchers/payload.rb
@@ -15,6 +15,8 @@ module JsonMatchers
     def extract_json_string(payload)
       if payload.respond_to?(:body)
         payload.body
+      elsif payload.is_a?(Hash)
+        payload.to_json
       else
         payload
       end
