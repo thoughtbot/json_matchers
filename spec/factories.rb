c45528c3dd05877cbb1122c6b4691660c009356b
Sean Doyle <sean.p.doyle24@gmail.com>
2018-03-16 13:35:05 -0400

    Declare `schema` factory
    This commit declares a FactoryBot factory for creating a JSON schema.
    
    Before the introduction of FactoryBot, the global `create_schema` test
    helper was responsible for turning a name and a JSON hash into a JSON
    Schema file, persisted to the filesystem.
    
    All `create_schema` call sites required a full JSON schema as its second
    argument.
    
    Most `create_schema` call sites share duplicated JSON fragments, and
    some covered similar or identical scenarios.
    
    With the addition of factories, there is an opportunity to declare
    [FactoryBot traits][traits] to abstract away the boring bits.
    
    [traits]: https://github.com/thoughtbot/factory_bot/blob/master/GETTING_STARTED.md#traits

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,5 +1,12 @@
+require "json_matchers/payload"
+
 FactoryBot.define do
   FakeResponse = Struct.new(:body)
+  FakeSchema = Struct.new(:name, :json) do
+    def to_s
+      name
+    end
+  end
 
   factory :response, class: FakeResponse do
     skip_create
@@ -9,6 +16,31 @@ FactoryBot.define do
       payload = attributes.except(:body)
 
       FakeResponse.new(body || payload.to_json)
+    end
+  end
+
+  factory :schema, class: FakeSchema do
+    sequence(:name) { |n| "json_schema-#{n}" }
+
+    trait :invalid do
+      json { "" }
+    end
+
+    skip_create
+
+    initialize_with do
+      name = attributes.fetch(:name)
+      json_attribute = attributes.fetch(:json, nil)
+      attributes_as_json = attributes.except(:json, :name)
+
+      FakeSchema.new(name, json_attribute || attributes_as_json)
+    end
+
+    after :create do |schema|
+      path = File.join(JsonMatchers.schema_root, "#{schema.name}.json")
+      payload = JsonMatchers::Payload.new(schema.json)
+
+      IO.write(path, payload)
     end
   end
 end
